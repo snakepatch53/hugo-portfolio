c612a5b4e933374b13449c3311f00d9dee98dae1
snakepatch53 <snakepatch53@gmail.com>
2024-05-14 03:44:51 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	renamed:    dist/assets/index-BP6F7R2f.css -> dist/assets/index-BnsguF7j.css
    	renamed:    dist/assets/index-T00KFRLL.js -> dist/assets/index-C7UYlqoV.js
    	modified:   dist/index.html
    	modified:   src/components/Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,12 +7,24 @@ import { useState } from "react";
 
 export default function Skills() {
     return (
-        <div id="skills" className="flex justify-center px-5 bg-black/10 py-20 ">
+        <div id="skills" className=" relative flex justify-center px-5 bg-black/10 mt-48 mb-32 ">
+            <svg viewBox="0 0 1440 320" className=" absolute bottom-full fill-black/10 ">
+                <path
+                    fillOpacity="1"
+                    d="M0,192L80,192C160,192,320,192,480,186.7C640,181,800,171,960,181.3C1120,192,1280,224,1360,240L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+                />
+            </svg>
+            <svg viewBox="0 0 1440 320" className=" absolute top-full fill-black/10 ">
+                <path
+                    fillOpacity="1"
+                    d="M0,256L120,234.7C240,213,480,171,720,154.7C960,139,1200,149,1320,154.7L1440,160L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
+                />
+            </svg>
             <div className=" flex flex-col items-center gap-10 w-full max-w-[--max-w] ">
                 <h1 className="font-title tracking-widest uppercase text-5xl text-[--c3] ">
                     Skills
                 </h1>
-                <div className=" grid grid-cols-3 gap-10 w-full max-w-[800px] ">
+                <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-10 w-full max-w-[800px] px-10 ">
                     <SkillItem icon={<FontAwesomeIcon icon={faHtml5} />} name="HTML" percent="80" />
                     <SkillItem icon={<FontAwesomeIcon icon={faCss3} />} name="CSS" percent="70" />
                     <SkillItem
